03f81f42ba71c03eebc1836d2680b26dfbc6ccf2
William A Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-02-16 08:38:49 -0600

    updated wrong solution

--- two_pointers/remove_dupes.py
+++ two_pointers/remove_dupes.py
@@ -40,21 +40,34 @@
 #     return nums
 
 # 2 pointer solution
-# for every unique item, left_pointer += 1
-# we use left pointer to keep track of positon as well of "left pointer"
-# we compare right pointer value to left pointer value.
-# if right pointer value == left pointer value, 
+
 def remove_dupes(nums):
+        # sorted array
+        # non decreasing order
+        # remove duplicates IN PLACE
+        # each element appears only once
+        # return number of unique elements
+        # [1,1,2,3,4]
+        # [1,2,3,4]
+        # return k = 4
+
+        # plan
+        # 2 pointer.
+        # left, starts at 1
+        # right, starts at 1
+        # left only iterates if right is a unique num
+        # why compare nums[right] to nums[right -1] and not to nums [left]
+        # left starts at 1. New unique values are set where left is.
+        # thats why we arent comparing to where left is.
+
     left = 1
-    # start right pointer at position 1, not 0
     for right in range(1, len(nums)):
         if nums[right] != nums[right - 1]:
-            # right is comparing if right is at a new number.
-            # if right is at a new number, incriment left
-            nums[left] = nums[right] 
+            nums[left] = nums[right]
             left += 1
-    return left
-        
+    print(left)
+    print(nums)
+    return left       
 
 
 
