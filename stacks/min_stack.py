eb1e8d677d7f9c1822448fc8b6cc5e60eca6a071
William A Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-02-17 14:48:19 -0600

    ezpz medium prob

--- stacks/min_stack.py
+++ stacks/min_stack.py
@@ -25,4 +25,28 @@
 #
 #     def getMin(self) -> int:
 #         return min(self.stack)
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        # if empty append first value
+        if not self.minstack:
+            self.minstack.append(val)
+        else:
+        # super ez pz check whats the min, append it
+        # minstack keeps track of the current lowest value seen
+            val_check = min(val, self.minstack[-1])
+            self.minstack.append(val_check)
 
+    def pop(self) -> None:
+        # pop the last val off of both, minstack only keeps the current lowest val
+        self.stack.pop()
+        self.minstack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+    
+    def getMin(self) -> int:
+        return self.minstack[-1]
