748d337ebefba43d7095ff5877c72f48ee062be8
William A Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-02-23 14:31:48 -0600

    updated with more performant solution

--- hashing/group_anagrams.py
+++ hashing/group_anagrams.py
@@ -12,15 +12,38 @@
 
 # I iterate through the list of strings once and add them to the hash and the new array at the same time
 
+
+
+# class Solution:
+#     def group_anagrams(self, strings: List[str]) -> List[List[str]]:
+#
+#         anagrams = defaultdict(list)
+#         for string in strings:
+#             sorted_strings = ''.join(sorted(string))
+#             anagrams[sorted_strings].append(string)
+#         return list(anagrams.values())
+
 from collections import defaultdict
-from typing import DefaultDict
 
 
+# performant solution in O(n * m) where m is the number of strings 
+# and n is the length of the longest string.
 class Solution:
-    def group_anagrams(self, strings: List[str]) -> List[List[str]]:
+     def group_anagrams(self, strings: List[str]) -> List[List[str]]:
+       
+        anagram_dicts = defaultdict(list)
+        for s in strs:
+#
+            count = [0] * 26
+            for character in s:
+                count[ord(character) - ord('a')] += 1
+# this is equivalent to
+# my_list = anagram_dicts[tuple(count)]
+# my_list.append(s)
+# tuples are used because they are immutable
+            anagram_dicts[tuple(count)].append(s)
+        return list(anagram_dicts.values())
 
-        anagrams = defaultdict(list)
-        for string in strings:
-            sorted_strings = ''.join(sorted(string))
-            anagrams[sorted_strings].append(string)
-        return list(anagrams.values())
+
+groupAnagrams(["acat","pots","topsss","cata","ssstop","hat"])
+
